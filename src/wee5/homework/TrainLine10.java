e01ca5f51c5364fa8f7acb068a5b7b3a3acf7e7b
anupatel14uk <132697950+anupatel14uk@users.noreply.github.com>
2023-06-04 19:38:24 +0100

    Update TrainLine10.java

--- src/wee5/homework/TrainLine10.java
+++ src/wee5/homework/TrainLine10.java
@@ -1,6 +1,7 @@
 package wee5.homework;
 
 import java.util.*;
+import java.util.HashMap;
 
 /**
  * Write a programme that tells you which line passes through particular stations.
@@ -8,36 +9,71 @@ import java.util.*;
  */
 public class TrainLine10 {
 
+
     public static void main(String[] args) {
-        TrainLine10 trainLine10 = new TrainLine10();
-        trainLine10.trainLines();
-    }
+        char result;
 
-    private void trainLines() {
-        Map<String, List<String>> stationMap = new HashMap<>();
+        System.out.println("This is Zone 1 Station list : ");
+        System.out.println("");
+        System.out.println("ALDGATE, EUSTON SQUARE, OXFORD CIRCUS, WATERLOO LU, TOWER HILL, OLD STREET,");
+        System.out.println("SOUTH KENSINGTON, VICTORIA LU, LANCASTER GATE, GREEN PARK, FARRINGDON, EDGWARE ROAD,");
+        System.out.println("QUEENSWAY, KINGS CROSS ST PANCRAS LU, VAUXHALL LU, EUSTON LU, BAYSWATER, ANGEL, PADDINGTON LU,");
+        System.out.println("BAKERSTREET, KNIGHTBRIDGE, CHARING CROSS LU, GLOUCESTER ROAD, REGENTS PARK, HYDE PARK CORNER,");
+        System.out.println("LEICESTER SQUARE, LONDON BRIDGE LU, PICCADILLY CIRCUS, MOORGATE, ST PAUL'S, WESTMINSTER, HOLBORN,");
+        System.out.println("COVENT GARDEN, BOND STREET, MARBLE ARCH, TOTTENHAM COURT ROAD, RUSSELL SQUARE,");
 
-        List<String> liverpoolList = new ArrayList<>();
-        liverpoolList.add("Central");
-        liverpoolList.add("Circle");
-        liverpoolList.add("Hammersmith & City");
-        liverpoolList.add("Metropolitan lines");
-        stationMap.put("Liverpool Street", liverpoolList);
+        System.out.println("");
 
-        List<String> bankList = new ArrayList<>();
-        bankList.add("Central");
-        bankList.add("Circle");
-        bankList.add("Hammersmith & City");
-        stationMap.put("Bank", bankList);
+        HashMap<String, String> zone = new HashMap<String, String>();
+        zone.put("ALDGATE", "Circle/Metropolitan");
+        zone.put("ANGEL", "Northern");
+        zone.put("BAKERSTREET", "Bakerloo/Circle/Hammersmith & City/Jubilee/Metropolitan");
+        zone.put("BAYSWATER", "Circle/District");
+        zone.put("BOND STREET", "Circle/Jubilee");
+        zone.put("CHARING CROSS LU", "Northern/Bakerloo");
+        zone.put("COVENT GARDEN", "Piccadilly");
+        zone.put("EDGWARE ROAD", "Circle/District/Hammersmith & City");
+        zone.put("EUSTON LU", "Victoria/Northern");
+        zone.put("EUSTON SQUARE", "Metropolitan/Hammersmith & City/Circle");
+        zone.put("FARRINGDON", "Circle/Hammersmith & City/Metropolitan");
+        zone.put("GLOUCESTER ROAD", "District/Circle/Piccadilly");
+        zone.put("GREEN PARK", "Jubilee/Piccadilly/Victoria");
+        zone.put("HOLBORN", "Central/Piccadilly");
+        zone.put("HYDE PARK CORNER", "Piccadilly");
+        zone.put("KINGS CROSS ST PANCRAS LU", "Circle/Piccadilly/Hammersmith & City/Northern/Metropolitan/Victoria");
+        zone.put("KNIGHTBRIDGE", "Piccadilly");
+        zone.put("LANCASTER GATE", "Central");
+        zone.put("LEICESTER SQUARE", "Northern/Piccadilly");
+        zone.put("LONDON BRIDGE LU", "Northern/Jubilee");
+        zone.put("MARBLE ARCH", "Central");
+        zone.put("MOORGATE", "Circle/Hammersmith & City/Metropolitan/Northern");
+        zone.put("OLD STREET", "Northern");
+        zone.put("OXFORD CIRCUS", "Bakerloo/Central/Victoria");
+        zone.put("PADDINGTON LU", "Hammersmith & City/Bakerloo/District/Circle");
+        zone.put("PICCADILLY CIRCUS", "Piccadilly/Bakerloo");
+        zone.put("QUEENSWAY", "Central");
+        zone.put("REGENTS PARK", "Bakerloo");
+        zone.put("RUSSELL SQUARE", "Piccadilly");
+        zone.put("SOUTH KENSINGTON", "District/Circle/Piccadilly");
+        zone.put("ST PAUL'S", "Central");
+        zone.put("TOTTENHAM COURT ROAD", "Central/Northern");
+        zone.put("TOWER HILL", "District/Circle");
+        zone.put("VAUXHALL LU", "Victoria");
+        zone.put("VICTORIA LU", "District/Circle");
+        zone.put("WATERLOO LU", "Bakerloo/Jubilee/Northern");
+        zone.put("WESTMINSTER", "Circle/District/Jubilee");
 
-        System.out.println("Enter station name : ");
-        Scanner scanner = new Scanner(System.in);
-        String stationName = scanner.next();
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter any station name from above list : \t");
 
-        System.out.println("Below lines are passes through " + stationName) ;
-        List<String> lineList = stationMap.get(stationName);
-        for (String line : lineList) {
-            System.out.println(line);
+            String name1 = input.nextLine();
+            name1 = name1.toUpperCase();
+
+            System.out.println("Tube Lines Available  : " + zone.get(name1));
+            System.out.println("Do you want to try again : 'Y' or 'N' ");
+            result = input.next().charAt(0);
         }
-
+        while (result == 'Y' || result == 'y');
     }
 }
